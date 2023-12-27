699962a366d850a772c7c2d6036e515ab661f518
Ezequiel Colicoy <ezequielcoly1@gmail.com>
2023-12-27 15:45:38 -0300

    registro

--- back/src/controller/logincontroller.js
+++ back/src/controller/logincontroller.js
@@ -7,9 +7,10 @@ import { generateToken } from "../middlewares/auth.middleware.js";
 export const signUp = async (req, res) => {
     const { username, email, password } = req.body;
 
-    if (!password || typeof password !== 'string') {
+    console.log(req.body);
+    /* if (!password || typeof password !== 'string') {
         return res.status(400).json({ error: "La contraseña es inválida" });
-    }
+    } */
     try {
 
         const userFind = await User.findOne({email: email})
