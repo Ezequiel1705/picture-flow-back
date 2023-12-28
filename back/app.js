51d298d7ce7cb55f08d9379df27c2782649124c7
Ezequiel Colicoy <ezequielcoly1@gmail.com>
2023-12-28 20:39:08 -0300

    cors

--- back/app.js
+++ back/app.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 
 const corsOptions = {
-  origin: process.env.ROOT_CLIENT, //YOU ROOT APP REACT/REACT NATIVE
+  origin: process.env.RAILWAY_PUBLIC_DOMAIN, //YOU ROOT APP REACT/REACT NATIVE
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
   optionsSuccessStatus: 204,
