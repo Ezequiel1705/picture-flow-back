40065113798d1e262df414085af6be4677a19865
Ezequiel Colicoy <ezequielcoly1@gmail.com>
2023-12-29 03:25:35 -0300

    app use

--- back/app.js
+++ back/app.js
@@ -1,24 +1,34 @@
 import fileUpload from 'express-fileupload';
 import router from './src/router/index.js';
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
 import "./options.js"
 import dotenv from 'dotenv';
 dotenv.config();
 
+const app = express()
 
-const corsOptions = {
+/* const corsOptions = {
   origin: process.env.ROOT_CLIENT, //YOU ROOT APP REACT/REACT NATIVE
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
   /* optionsSuccessStatus: 204,
-  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Headers", "url"], */
-};
+  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Headers", "url"], 
+}; */
 
-const app = express()
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://picture-flow-front-production.up.railway.app");
+  
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+    next();
+  });
+
+
 
 app.use(express.json());
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', router)
